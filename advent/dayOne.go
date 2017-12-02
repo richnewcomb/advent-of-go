2d5a2de0f2e6fd70c0b197929de15c652cc63b52
Rich Newcomb <rich.newcomb@walmart.com>
2017-12-02 00:49:53 -0500

    Updated readme

---
+++ advent/dayOne.go
@@ -0,0 +1,32 @@
+package advent
+
+import (
+"fmt"
+"strconv"
+)
+
+func CalcCaptcha(captcha string, step int)(int){
+	fmt.Println("December 1")
+	fmt.Println(captcha);
+
+	position := 0
+	nextPosition := 0
+	currentValue := 0
+	nextValue := 0
+	checkSum := 0
+
+	for position < len(captcha) {
+		currentValue, _ = strconv.Atoi(string(captcha[position]));
+		if (position + step < len(captcha)) {
+			nextPosition = position + step;
+		} else {
+			nextPosition = (position + step) - len(captcha);
+		}
+		nextValue, _ = strconv.Atoi(string(captcha[nextPosition]));
+		if (currentValue == nextValue) {
+			checkSum += currentValue;
+		}
+		position++;
+	}
+	return checkSum;
+}
