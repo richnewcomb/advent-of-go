59eed51ce958c6aabaaaac2433e47d538215a86c
Rich Newcomb <rich.newcomb@walmart.com>
2017-12-02 15:15:35 -0500

    Cleaned code

---
+++ adventUtil.go
@@ -0,0 +1,43 @@
+package advent
+
+import (
+	"path/filepath"
+	"os"
+	"fmt"
+	"encoding/csv"
+	"strconv"
+)
+
+// Read and return all of the records in a file
+func ReadTsvRecords(filename string) [][]string {
+	absPath, _ := filepath.Abs(filename)
+	file, err := os.Open(absPath)
+	if err != nil {
+		fmt.Println("Error reading intput file:", err)
+		return nil
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	//Configure reader options Ref http://golang.org/src/pkg/encoding/csv/reader.go?s=#L81
+	reader.Comma = '\t'           // Use tab-delimited instead of comma
+	reader.Comment = '#'          //Comment character
+	reader.FieldsPerRecord = -1   //Number of records per record. Set to Negative value for variable
+	reader.TrimLeadingSpace = true
+
+	records, err := reader.ReadAll()
+	return records
+}
+
+// Convert [][]string into [][]int to eliminate string to int conversions elsewhere
+func ReadTsvRecordsToInt(filename string) [][]int {
+	records := make([][]int, 0)
+	strRecords := ReadTsvRecords(filename)
+	for i:= 0; i < len(strRecords); i++ {
+		records = append(records, make([]int, len(strRecords[i])))
+		for j:=0; j < len(strRecords[i]); j++ {
+			records[i][j], _ = strconv.Atoi(strRecords[i][j])  // the error handling is certainly bad
+		}
+	}
+	return records
+}
