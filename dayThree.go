3458240d3c64315d655d3e2b5560be7d3ae81d9f
Rich Newcomb <rich.newcomb@gmail.com>
2017-12-03 12:11:43 -0500

    Day three raw code

--- dayThree.go
+++ dayThree.go
@@ -1,1 +1,80 @@
 package advent
+
+import (
+	"strconv"
+)
+
+var gridMap = make(map[string]int)
+
+func CalcManhattanDistance(location int) int {
+	i := 1;
+	ring := 0;
+	for i < location {
+		ring++;
+		for side := 0; side < 4; side++ {
+			sideLen := ring * 2;
+		    for j := sideLen; j > sideLen / 2; j-- {
+		    	i++
+				if i == location { return j -1 }
+			}
+			for k := 1; k <= sideLen / 2; k++ {
+				i++
+				if i == location { return k + ring }
+			}
+		}
+	}
+	return -1;
+}
+
+func SumAdjacentLocations(minTotal int) int {
+
+	gridMap[strconv.Itoa(0) + "," + strconv.Itoa(0)] = 1
+
+	value := 0;
+	x := 0;
+	y := -1;
+
+	for value < minTotal {
+		x++;
+		sideLen := x * 2;
+		for i := 0; i < sideLen; i++ {
+			y++;
+			value = sumAdjacentLocations(x, y);
+			if value > minTotal {return value}
+		}
+		for i := 0; i < sideLen; i++ {
+			x--;
+			value = sumAdjacentLocations(x, y);
+			if value > minTotal {return value}
+		}
+		for i := 0; i < sideLen; i++ {
+			y--;
+			value = sumAdjacentLocations(x, y);
+			if value > minTotal {return value}
+		}
+		for i := 0; i < sideLen; i++ {
+			x++;
+			value = sumAdjacentLocations(x, y);
+			if value > minTotal {return value}
+		}
+		y--;
+	}
+
+	return -1;
+}
+
+func sumAdjacentLocations(x int, y int) int {
+	sum :=
+		gridMap[strconv.Itoa(x) + "," + strconv.Itoa(y + 1)] +
+		gridMap[strconv.Itoa(x) + "," + strconv.Itoa(y - 1)] +
+		gridMap[strconv.Itoa(x + 1) + "," + strconv.Itoa(y)] +
+		gridMap[strconv.Itoa(x - 1) + "," + strconv.Itoa(y)] +
+	    gridMap[strconv.Itoa(x + 1) + "," + strconv.Itoa(y + 1)] +
+	    gridMap[strconv.Itoa(x - 1) + "," + strconv.Itoa(y - 1)] +
+	    gridMap[strconv.Itoa(x + 1) + "," + strconv.Itoa(y - 1)] +
+		gridMap[strconv.Itoa(x - 1) + "," + strconv.Itoa(y + 1)]
+
+	gridMap[strconv.Itoa(x) + "," + strconv.Itoa(y)] = sum
+
+	return sum;
+}
--- dayThree_test.go
+++ dayThree_test.go
@@ -1,1 +1,17 @@
 package advent
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestCalcManhattanDistance(t *testing.T) {
+	distance := CalcManhattanDistance(265149)
+	fmt.Printf("%d", distance)
+}
+
+func TestSumAdjacentLocations(t *testing.T) {
+	sumOfAdjacentLocations := SumAdjacentLocations(265149)
+	fmt.Printf("%d", sumOfAdjacentLocations)
+
+}
